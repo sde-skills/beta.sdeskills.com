ee4a3f97446a3537708c97e65a41cc8de3e136b0
Vivek K <vivek@sdeskills.com>
2019-11-04 07:13:11 -0800

    another round of updates

---
+++ a.md
@@ -0,0 +1,4 @@
+---
+permalink: /a
+redirect_to: 'http://example.com'
+---
--- assets/scripts.js
+++ assets/scripts.js
@@ -1,3 +1,25 @@
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 })
+
+$(function() {
+        a("#contactForm").submit(function(c) {
+            c.preventDefault();
+            a("#contactSubmit").html("Wait..").attr("disabled", "disabled");
+            var b = a.ajax({
+                url: "/scripts/signup.php",
+                type: "post",
+                data: a("#contactForm").serialize(),
+                success: function(d) {
+                    a("#contactSubmit").html("Subscribed...").attr("disabled", "disabled")
+                },
+                error: function(d) {
+                    a("#contactSubmit").html("Failed...").attr("disabled", "disabled");
+                    setTimeout(function() {
+                        a("#contactSubmit").html("Subscribe").prop("disabled", false)
+                    }, 2000)
+                }
+            })
+        })
+})
+
