a06a7c7fead7aa3840fc07c0f8d2225b85b1b516
Vivek K <vivek@sdeskills.com>
2019-11-09 08:54:43 -0800

    Updated URL of the script/signup

--- assets/scripts.js
+++ assets/scripts.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
 	   		e.preventDefault();
        $("button#contactSubmit").html("Wait..").attr("disabled", "disabled");	   		
         var b = $.ajax({
-                url: "/scripts/signup.php",
+                url: "https://www.sdeskills.com/scripts/signup.php",
                 type: "post",
                 data: $("#contactForm").serialize(),
                 success: function(d) {
